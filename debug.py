b53bbce8cfffbcf926f5b9951cb89be9cdb8b276
Michael Aherne <miketwo@gmail.com>
2015-04-22 01:48:06 -0700

    update debugger

--- debug.py
+++ debug.py
@@ -3,12 +3,28 @@ from time import time
 
 START = 0
 FINISH = 0
+ACCUMULATOR = 0
+DELTA = 0
+
 
 def start():
-    global START
+    global START, DELTA
     START = time()
+    DELTA = START
+
 
 def finish():
-    global FINISH
-    FINISH = time()
-    print FINISH - START
+    t = time()
+    if ACCUMULATOR != 0:
+        print "Acumulated time: {}".format(ACCUMULATOR)
+    else:
+        global FINISH
+        FINISH = t
+        print "Total time: {}".format(FINISH - START)
+
+
+def accumulate():
+    global ACCUMULATOR, DELTA
+    t = time()
+    ACCUMULATOR += t - DELTA
+    DELTA = t
