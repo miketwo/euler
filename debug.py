fb729e1acbf447f6d5339841559a1a736d1888a7
Michael Aherne <miketwo@gmail.com>
2015-04-23 11:20:14 -0700

    more debugging timers

--- debug.py
+++ debug.py
@@ -1,4 +1,11 @@
 # Useful debug
+'''
+Author: Michael Ricks-Aherne
+
+This file contains a bunch of useful timing functions for profiling code.
+They are not as extensive or accurate as Python's timeit or profiler, but are
+meant to be used quickly for a rough estimate.
+'''
 from time import time
 
 START = 0
@@ -28,3 +35,13 @@ def accumulate():
     t = time()
     ACCUMULATOR += t - DELTA
     DELTA = t
+
+
+# Recipe from http://code.activestate.com/recipes/578776-a-simple-timing-function/
+# Modified slightly for more accuracy
+def dur(op=None, clock=[time()]):
+    t = time()
+    if op is not None:
+        duration = t - clock[0]
+        print '%s finished. Duration %.6f seconds.' % (op, duration)
+    clock[0] = t
